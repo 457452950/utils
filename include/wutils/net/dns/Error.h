5cc409102f108481f831f9927f5f88e59d3df727
良斌 汪 <1602434826@qq.com>
2023-08-12 23:32:52 +0800

    [feat]dns complete.

--- include/wutils/net/dns/Error.h
+++ include/wutils/net/dns/Error.h
@@ -2,52 +2,26 @@
 #ifndef WUTILS_NET_DNS_ERROR_H
 #define WUTILS_NET_DNS_ERROR_H
 
-#include <unordered_map>
+#include <netdb.h>
 
 #include "wutils/Error.h"
+#include "wutils/network/base/Definition.h"
 
 namespace wutils::net::dns {
 
-enum class eNetDNSError : int { OK = 0, SYSTEM_HOSTS_FILE_NOT_EXIST, HOSTS_FILE_PARSE_ERROR, UNKNOWN = INT32_MAX };
-
-#define ERRMAPITEM(i, v)                                                                                               \
-    { static_cast<int>(eNetDNSError::i), (char *)v }
-
-const inline std::unordered_map<int, char *> ErrorMessageMap = {
-        ERRMAPITEM(OK, "ok"),
-        ERRMAPITEM(SYSTEM_HOSTS_FILE_NOT_EXIST, "system hosts file is not exist."),
-        ERRMAPITEM(HOSTS_FILE_PARSE_ERROR, "hosts file parse error."),
-        ERRMAPITEM(UNKNOWN, "unknown error")};
-
-
-class NetWorkErrorCategory : public error_category {
+class NetDNSErrorCategory : public error_category {
 public:
-    NetWorkErrorCategory() = default;
+    NetDNSErrorCategory() = default;
     const char *name() const noexcept override { return "dns"; }
-    std::string message(int value) const override {
-        static const auto unkown = ErrorMessageMap.find(static_cast<int>(eNetDNSError::UNKNOWN));
-
-        auto it = ErrorMessageMap.find(value);
-        if(it == ErrorMessageMap.end()) {
-            return unkown->second;
-        }
-
-        return it->second;
-    }
+    std::string message(int value) const override { return gai_strerror(value); }
 };
 
-
-Error make_error_code(eNetDNSError error) {
-    static NetWorkErrorCategory category;
+Error make_error_code(int error) {
+    static NetDNSErrorCategory category;
     return {static_cast<int>(error), category};
 }
 
-
 } // namespace wutils::net::dns
 
-namespace std {
-template <>
-struct is_error_code_enum<wutils::net::dns::eNetDNSError> : public true_type {};
-} // namespace std
 
-#endif // WUTILS_NET_DNS_ERROR_H
+#endif // INCLUDE_WUTILS_NET_DNS_ERROR_H
--- test/test_net_dns.cpp
+++ test/test_net_dns.cpp
@@ -1,13 +1,65 @@
 #include <gtest/gtest.h>
 
-#include "wutils/net/dns/HostsFile.h"
+#include "wutils/net/dns/DNS.h"
 
-
-TEST(ssl, parse) {
+TEST(dns, dns) {
     using namespace wutils::net;
-    auto map = dns::ParseSystemHosts();
-    for(const auto &it : map) {
-        GTEST_LOG_(INFO) << "host name : " << it.first << "  host ip : " << it.second;
+    auto future = dns::DNS::Request("www.baidu.com");
+
+    auto res = future.get();
+    GTEST_LOG_(INFO) << res.Success();
+    for(auto item : res.Get()) {
+        auto [ip, port] = item.net_address.Dump();
+        GTEST_LOG_(INFO) << item.protocol << " " << item.socket_type << " " << item.name << " " << ip << " " << port
+                         << std::endl;
     }
-    GTEST_LOG_(INFO) << "map size " << map.size();
+    GTEST_LOG_(INFO) << res.Get().size();
+
+    future = dns::DNS::Request("www.baidu.com", dns::BOTH, dns::TCP);
+    res    = future.get();
+    GTEST_LOG_(INFO) << res.Success();
+    for(auto item : res.Get()) {
+        auto [ip, port] = item.net_address.Dump();
+        GTEST_LOG_(INFO) << item.protocol << " " << item.socket_type << " " << item.name << " " << ip << " " << port
+                         << std::endl;
+    }
+    GTEST_LOG_(INFO) << res.Get().size();
+
+    future = dns::DNS::Request("www.baidu.com", dns::INET6);
+    res    = future.get();
+    GTEST_LOG_(INFO) << res.Success();
+    for(auto item : res.Get()) {
+        auto [ip, port] = item.net_address.Dump();
+        GTEST_LOG_(INFO) << item.protocol << " " << item.socket_type << " " << item.name << " " << ip << " " << port
+                         << std::endl;
+    }
+    GTEST_LOG_(INFO) << res.Get().size();
+
+    future = dns::DNS::Request("www.baidududu.com");
+    res    = future.get();
+    GTEST_LOG_(INFO) << res.Success();
+    if(!res.Success()) {
+        GTEST_LOG_(INFO) << res.GetError().message();
+    } else {
+        for(const auto &item : res.Get()) {
+            auto [ip, port] = item.net_address.Dump();
+            GTEST_LOG_(INFO) << item.protocol << " " << item.socket_type << " " << item.name << " " << ip << " " << port
+                             << std::endl;
+        }
+    }
+    GTEST_LOG_(INFO) << res.Get().size();
+
+    future = dns::DNS::Request("www.google.com");
+    res    = future.get();
+    GTEST_LOG_(INFO) << res.Success();
+    if(!res.Success()) {
+        GTEST_LOG_(INFO) << res.GetError().message();
+    } else {
+        for(const auto &item : res.Get()) {
+            auto [ip, port] = item.net_address.Dump();
+            GTEST_LOG_(INFO) << item.protocol << " " << item.socket_type << " " << item.name << " " << ip << " " << port
+                             << std::endl;
+        }
+    }
+    GTEST_LOG_(INFO) << res.Get().size();
 }
